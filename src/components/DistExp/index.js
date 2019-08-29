537cd5bb4e08136d99cd54d09dca62def8d50a44
renzoMoraS <rmora@alumno.huergo.edu.ar>
2019-08-28 21:50:27 -0300

    Metiendo lo de gena

---
+++ src/components/DistExp/index.js
@@ -0,0 +1,92 @@
+import React, { Component } from 'react';
+
+var contgs = 0
+var contgpro = 0
+var contgp = 0
+var contsil = 0
+var contbr = 0
+var contfree = 0
+var contgold = 0
+class DistExp extends Component{
+    constructor(props){
+        super(props);
+        this.state = {termino:false};
+    }
+
+    uData(){
+
+        contgs = 0
+        contgpro = 0
+        contgp = 0
+        contsil = 0
+        contbr = 0
+        contfree = 0
+        contgold = 0
+
+        var userdata = JSON.parse(localStorage.getItem('clientsOrders'));
+        for (var i = 0; i < userdata.results.length; i++) {
+            var expo = userdata.results[i].order_items[0].listing_type_id;
+            switch(expo){
+                case 'gold_pro':
+                    console.log('gold_pro');
+                    contgpro ++;
+                    console.log(contgpro);
+                    break;
+                case 'gold_special':
+                    console.log('gold_special');
+                    contgs ++;
+                    console.log(contgs);
+                    break;
+                case 'gold':
+                    console.log('gold');
+                    contgold ++;
+                    console.log(contgold);
+                    break;
+                case 'silver':
+                    console.log('silver');
+                    contsil ++;
+                    console.log(contsil);
+                    break;
+                case 'bronze':
+                    console.log('bronze');
+                    contbr ++;
+                    console.log(contbr);
+                    break;
+                case 'free':
+                    console.log('free');
+                    contfree ++;
+                    console.log(contfree);
+                    break;
+                case 'gold_premium':
+                    console.log('gold_premium');
+                    contgp ++;
+                    console.log(contgp);
+                    break;
+                default:
+                    break;
+            }
+        }
+        this.setState({termino:true});
+        this.setState({termino:false});
+    }
+    
+    render(){
+        return (
+            <div className="DistExp">
+                <h1 style={{textAlign: 'center'}}>Distribucion por exposición</h1>
+                <div className="patient-container">
+                    <button onClick={this.uData.bind(this)}>Tocame perro</button>
+                    <p>Ventas de cada tipo de publicacion</p>
+                    <p>gold_premium: {contgp}</p>
+                    <p>gold_pro: {contgpro}</p>
+                    <p>gold_special: {contgs}</p>
+                    <p>gold: {contgold}</p>
+                    <p>silver: {contsil}</p>
+                    <p>bronze: {contbr}</p>
+                    <p>free: {contfree}</p>
+                </div>
+            </div>
+        );
+    }
+}
+export default (DistExp);
--- src/routes.js
+++ src/routes.js
@@ -6,6 +6,7 @@ import { Route, Switch } from 'react-router-dom';
 import App from './components/App';
 import BMap from './components/BMap';
 import CatTime from './components/CatTime';
+import DistExp from './components/DistExp';
 import LoguedIn from './components/LoguedIn';
 import Home from './components/Home';
 import Page404 from './components/Page404';
@@ -15,6 +16,7 @@ const AppRoutes = () =>
         <Switch>
             <Route exact path="/bmap" component={BMap} />
             <Route exact path="/cattime" component={CatTime} />
+            <Route exact path="/distexp" component={DistExp} />
             <Route exact path="/logued_in" component={LoguedIn} />
             <Route exact path="/" component={Home} />
             <Route component={Page404} />
